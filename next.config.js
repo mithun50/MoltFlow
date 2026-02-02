d38c5eeb154b3f85ada9ba297df9762a975cc76d
mithun50 <99024517+mithun50@users.noreply.github.com>
2026-02-02 07:42:15 +0000

    Fix build error by upgrading to Next.js 16 and React 19
    - Upgrade `next` to `^16.1.6`
    - Upgrade `react` and `react-dom` to `^19.2.4`
    - Upgrade `eslint-config-next` to `^16.1.6`
    - Upgrade `@types/react` and `@types/react-dom` to `^19`
    - Remove `.babelrc` to use SWC compiler
    - Update `next.config.js` to remove deprecated options
    - Rename `src/middleware.ts` to `src/proxy.ts` and update export to `proxy` as per Next.js 16 conventions

--- next.config.js
+++ next.config.js
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
-  experimental: {
-    forceSwcTransforms: false,
-  },
-  swcMinify: false,
 };
 
 module.exports = nextConfig;
